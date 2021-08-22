791a610c8f67882eaab5f1c46a2d91af7b919938
pws-stepsoft <pshields@stepsoft.com.au>
2021-08-23 09:42:55 +1000

    Add staff to roles

--- src/data/staff.js
+++ src/data/staff.js
@@ -1,7 +1,16 @@
 const staff = [
-    {id: 1, title: "Jimmy Durante", eventColor: ''},
-    {id: 2, title: "Harry Belafonte", eventColor: ''},
-    {id: 3, title: "Doris Day", eventColor: ''}
+    {
+        id: "r1", title: "Physiotherapist",
+        children:
+            [
+                {id: 1, title: "Jimmy Durante", eventColor: ''}
+            ]
+    },
+    {
+        id: "r2", title: "Care provider",
+        children: [{id: 2, title: "Harry Belafonte", eventColor: ''},
+            {id: 3, title: "Doris Day", eventColor: ''}]
+    }
 ];
 
 export default staff;
