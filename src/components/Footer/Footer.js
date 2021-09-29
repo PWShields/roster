7c8ee7adbefd4bf434ef804aa0930c0b5ac84f29
pws-stepsoft <pshields@stepsoft.com.au>
2021-09-30 08:36:39 +1000

    CM-127: remove footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,8 @@ const Footer = () => {
         <div className="footer">
             <hr style={{width: "90%", marginTop: 20}}/>
             <span className="name">
-        Made by{" "}
-                <a href="https://www.stepsoft.com.au/" target="__blank">
-          Stepsoft
-        </a>
-      </span>
+            </span>
             <div className="iconContainer">
-
             </div>
         </div>
     );
